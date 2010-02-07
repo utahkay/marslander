1c4c8f876f7eb3aec15c5a3cb222ec4ca23756e7
Kay Johansen <kay@kayjohansen.org>
2010-02-06 18:13:56 -0700

    Refactored lander logic into LanderController

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,25 +1,22 @@
 package game;
 
+import game.sprites.Astronaut;
+import game.ui.GameFrame;
+import game.ui.GameUI;
+import sprites.SpriteContainer;
+import sprites.SpriteImageManager;
+import sprites.SpriteVector;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.ImageObserver;
-
-import sprites.SpriteImageManager;
-import sprites.SpriteVector;
-import sprites.SpriteContainer;
-import game.ui.GameFrame;
-import game.ui.GameUI;
-import game.sprites.Astronaut;
-import game.sprites.MarsLander;
 
 public class Game implements SpriteContainer {
     private final WinLossStatsController winLossStats;
     private final GameUI uiComponent;
     private final Rectangle bounds;
-    private MarsLander lander;
+    private LanderController landerController;
     private Astronaut astronaut;
-    private boolean isJetFiring;
-    private static final double JET_ACCELERATION = 0.005;
     private static final double CRASH_VELOCITY = 0.2;
 
     public static void main(String[] args) {
@@ -36,29 +33,23 @@ public class Game implements SpriteContainer {
     }
 
     public void keyPressed() {
-        if (!isJetFiring && astronaut == null) {
-            jetOn();
+        if (astronaut == null) {
+            landerController.jetOn();
         }
     }
 
     public void keyReleased() {
-        if (isJetFiring) {
-            jetOff();
-        }
+        landerController.jetOff();
     }
 
     public void startNewGame() {
         stopSprites();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e1) {
-            Thread.currentThread().interrupt();
-        }
+        waitOneSecond();
         startGame();
     }
 
     public void paintSprites(Graphics g, ImageObserver observer) {
-        lander.paint(g, observer);
+        landerController.paint(g, observer);
         if (astronaut != null) {
             astronaut.paint(g, observer);
         }
@@ -76,42 +67,20 @@ public class Game implements SpriteContainer {
 
     private void startGame() {
         astronaut = null;
-        lander = createLanderSprite();
-        setInitalParameters();
-        lander.start();
+        landerController = new LanderController(this, bounds);
         uiComponent.requestFocus();
     }
 
-    private void setInitalParameters() {
-        isJetFiring = false;
-        lander.showLander();
-        lander.setPosition(new SpriteVector(100.0, 100.0));
-        lander.setVelocity(new SpriteVector(0.095, 0.0));
-        lander.setAcceleration(new SpriteVector(0.0, 0.003)); // Gravity
-        lander.setCollisionLoss(0.95);
-    }
-
-    private void jetOn() {
-        isJetFiring = true;
-        lander.showLanderWithFlame();
-        lander.changeAcceleration(new SpriteVector(0, -JET_ACCELERATION));
-    }
-
-    private void jetOff() {
-        isJetFiring = false;
-        lander.showLander();
-        lander.changeAcceleration(new SpriteVector(0, JET_ACCELERATION));
-    }
-
-    private MarsLander createLanderSprite() {
-        MarsLander sprite = new MarsLander(new SpriteImageManager(), this);
-        sprite.setBounds(bounds);
-        return sprite;
+    private void waitOneSecond() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            Thread.currentThread().interrupt();
+        }
     }
 
     private Astronaut createAstronautSprite() {
-        SpriteVector position = lander.getPosition();
-        position.x += lander.getSpriteSize()/2;
+        SpriteVector position = landerController.startingPositionForAstronaut();
         Astronaut sprite = new Astronaut(new SpriteImageManager(), this);
         sprite.setBounds(bounds);
         sprite.setPosition(position);
@@ -120,26 +89,24 @@ public class Game implements SpriteContainer {
     }
 
     private void stopSprites() {
-        lander.requestStop();
+        landerController.requestStop();
         if (astronaut != null) {
             astronaut.requestStop();
         }
     }
 
     private void stopGame(boolean didPlayerWin) {
-        if (isJetFiring) {
-            jetOff();
-        }
+        landerController.jetOff();
         if (didPlayerWin) {
             showWin();
         } else {
             showLoss();
         }
-        lander.requestStop();
+        landerController.requestStop();
     }
 
     private void showLoss() {
-        lander.showCrash();
+        landerController.crash();
         winLossStats.gameLost();
     }
 
---
+++ src/game/LanderController.java
@@ -0,0 +1,66 @@
+package game;
+
+import game.sprites.MarsLander;
+import sprites.SpriteContainer;
+import sprites.SpriteImageManager;
+import sprites.SpriteVector;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
+public class LanderController {
+    private MarsLander lander;
+    private boolean isJetFiring;
+    private static final double JET_ACCELERATION = 0.005;
+
+    public LanderController(SpriteContainer spriteContainer, Rectangle bounds) {
+        lander = new MarsLander(new SpriteImageManager(), spriteContainer);
+        lander.setBounds(bounds);
+        setInitalParameters();
+        lander.start();
+    }
+
+    public void jetOn() {
+        if (!isJetFiring) {
+            isJetFiring = true;
+            lander.showLanderWithFlame();
+            lander.changeAcceleration(new SpriteVector(0, -JET_ACCELERATION));
+        }
+    }
+
+    public void jetOff() {
+        if (isJetFiring) {
+            isJetFiring = false;
+            lander.showLander();
+            lander.changeAcceleration(new SpriteVector(0, JET_ACCELERATION));
+        }
+    }
+
+    public void requestStop() {
+        lander.requestStop();
+    }
+
+    public void crash() {
+        lander.showCrash();
+    }
+
+    public void paint(Graphics g, ImageObserver observer) {
+        lander.paint(g, observer);
+    }
+
+    public SpriteVector startingPositionForAstronaut() {
+        SpriteVector position = lander.getPosition();
+        position.x += lander.getSpriteSize()/2;
+        return position;
+    }
+
+    private void setInitalParameters() {
+        isJetFiring = false;
+        lander.showLander();
+        lander.setPosition(new SpriteVector(100.0, 100.0));
+        lander.setVelocity(new SpriteVector(0.095, 0.0));
+        lander.setAcceleration(new SpriteVector(0.0, 0.003)); // Gravity
+        lander.setCollisionLoss(0.95);
+    }
+
+}
